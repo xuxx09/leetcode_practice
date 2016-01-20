d606e31c95df7ddb1b9f8f9bb903725d7099571c
xuxx <xuxx09@126.com>
2016-01-20 17:08:53 +0800

    modify the 01TwoSum.cpp

--- 01TwoSum.cpp
+++ 01TwoSum.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <vector>
+#include <map>
 using namespace std;
 
-int main()
+vector<int> twoSum(vector<int> &nums, int target)
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    vector<int> res;
+    map<int, int> h;
+    for(int i=0; i<nums.size(); i++)
+    {
+        if(h.find(target-nums[i])!= h.end())//使用map进行处理，寻找已经遍历元素是否能和当前元素配对
+        {
+            int index=h[target-nums[i]];
+            res.push_back( min(i+1,index+1) );
+            res.push_back( max(i+1,index+1) );
+            break;
+        }
+        h.insert(pair<int, int>(nums[i],i));
+        //h.insert(make_pair(nums[i], i));
+    }
+    return res;
 }
