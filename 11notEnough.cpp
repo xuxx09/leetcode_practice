c39272efc9459326e886e08d4206d7bfa5d2156d
xuxx <xuxx09@126.com>
2016-01-28 17:52:12 +0800

    add an easy way to solve the problem, but the time is limit

--- 11notEnough.cpp
+++ 11notEnough.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-int maxArea(vector<int>& height) {
+int maxArea_01(vector<int>& height) {
     int water = 0;
     int i = 0, j = height.size() - 1;
     while (i < j) {
@@ -9,6 +9,26 @@ int maxArea(vector<int>& height) {
         water = max(water, (j - i) * h);
         while (height[i] <= h && i < j) i++;
         while (height[j] <= h && i < j) j--;
+    }
+    return water;
+}
+//提交后显示超时。
+int maxArea(vector<int>& height)
+{
+    int water = 0;
+    int h,l;
+    for(int i = 0; i < height.size(); i++)
+    {
+        h = height[i];
+        l = i;
+        for(int j=i+1; j<height.size();j++)
+        {
+            if(height[j]>=height[i])
+            {
+                l=j;
+            }
+        }
+        water = max(water, (l-i)*h);
     }
     return water;
 }
