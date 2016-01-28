78345a1051e0d6331a5f6be26fb9742559783b9b
xuxx <xuxx09@126.com>
2016-01-28 18:17:27 +0800

    add a new problem

---
+++ 12IntegertoRoman.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+string intToRoman(int num)
+{
+    string M[] = {"", "M", "MM", "MMM"};
+    string C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    string X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    string I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[(num%10)];
+}
+
+
+
+
+int main()
+{
+
+    cout <<intToRoman(120)<< endl;
+    return 0;
+}
